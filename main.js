74ad7f0bd04d692948bf551b8be117397649de60
Enmanuel <54204155+emtes@users.noreply.github.com>
2020-03-03 10:22:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-var SpotifyWebApi = require('spotify-web-api-node');
-
 console.log("Hello, world!");
 const params = new URLSearchParams(window.location.hash);
 const token = params.get("#access_token");
 console.log(token);
-
-var spotifyApi = new SpotifyWebApi()
-spotifyApi.setAccessToken(token);
