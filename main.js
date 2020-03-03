cf01762c508e7a93f7cd4fa5eee2fa8131886642
Enmanuel <54204155+emtes@users.noreply.github.com>
2020-03-03 00:21:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-body.addEventListener("onload", () => {
+window.addEventListener("load", () => {
   console.log("Hello, world!");
   const params = new URLSearchParams(window.location.hash);
   const token = params.get("#access_token");
