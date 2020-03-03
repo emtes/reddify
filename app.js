61f6411bee50e0bce5996dcf91692c8bdd421b1f
Enmanuel <54204155+emtes@users.noreply.github.com>
2020-03-03 11:36:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function toSpotifyAccServices() {
     client_id: "20072dfe69e4443ab2981969b901cbae",
     response_type: "token",
     redirect_uri: "https%3A%2F%2Femtes.github.io%2Ftest%2Fmain.html",
-    scope: "user-top-read"
+    scope: "user-top-read%20user-library-read"
   };
 
   let reqAuth = `https://accounts.spotify.com/authorize?client_id=${creds.client_id}&redirect_uri=${creds.redirect_uri}&response_type=${creds.response_type}&scope=${creds.scope}`;
