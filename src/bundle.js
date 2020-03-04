ba2d7f18ab7e0db9f0d4b1a557d8fd8696a07c14
Deteriator <markgriffith009@gmail.com>
2020-03-04 09:47:50 -0500

    changed variable keyword.

--- src/bundle.js
+++ src/bundle.js
@@ -41,13 +41,13 @@
         /*
 --------------------------------------------------------------------------------
 */
-        var SpotifyWebApi = require('spotify-web-api-node')
+        const SpotifyWebApi = require('spotify-web-api-node')
 
         const params = new URLSearchParams(window.location.hash)
         const token = params.get('#access_token')
         console.log(token)
 
-        var spotifyApi = new SpotifyWebApi({
+        let spotifyApi = new SpotifyWebApi({
           clientId: '20072dfe69e4443ab2981969b901cbae',
           clientSecret: '6e911053d5cf42f09fbd334d66594e49',
           redirectUri: 'https://emtes.github.io/test/main'
