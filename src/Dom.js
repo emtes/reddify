7f6b6c910986acfd00da7b15c02c8bdfb24ed20e
Enmanuel <edelanuezlang@gmail.com>
2020-03-04 20:08:47 -0500

    add markup str

--- src/Dom.js
+++ src/Dom.js
@@ -2,11 +2,27 @@ class Dom {
   constructor() {}
 
   static drawSongCards(songsArr) {
-    songsArr.forEach((song) => {
-      let cardHtml= ``;
-    })
 
+    songsArr.forEach(song => {
+    	let cardHtml = `
+		<div class="card">
+      		<div class="card-image">
+        		<img src="#" alt="[[SONG COVER ART]]" class="img-responsive">
+      		</div>
+
+      		<div class="card-header">
+        		<div class="card-title h5">[[SONG NAME]]</div>
+      			<div class="card-subtitle text-gray">[[ARTIST]]</div>
+      		</div>
+  
+      		<div class="card-footer">
+        		<button class="btn btn-primary">See in Spotify</button>
+      		</div>
+    	</div>
+      `;
+    });
   }
+
 
 
 }
