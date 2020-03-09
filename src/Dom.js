63ca11cad1cf36fe45ddf8dcfbbd884d057491ef
Deteriator <markgriffith009@gmail.com>
2020-03-08 22:12:09 -0400

    changed grid layout.

--- src/Dom.js
+++ src/Dom.js
@@ -2,13 +2,27 @@ class Dom {
   constructor() {}
 
   static drawSongCards(songsArr) {
-    songsArr.forEach(song => {
-      let cardHtml = ``;
-    });
+  	return `
+  	<div class="card column col-3">
+	  <div class="card-image">
+	    <img src="${card.icon}" class="img-responsive">
+	  </div>
+	  <div class="card-header">
+	    <div class="card-title h5">${card.commNamePrefixed}</div>
+	    <div class="card-subtitle text-gray">Subscribers ${card.subscriberCount}</div>
+	  </div>
+	  <div class="card-body">
+		${card.description}
+	  </div>
+	  <div class="card-footer">
+	    <a href="${card.url}"><button class="btn btn-primary">To Subreddit</button></a>
+	  </div>
+	</div>`
+
   }
   static createRedditCard(card) {
     return `
-	  <div class="card column col-3">
+	  <div class="card column col-6">
 	  <div class="card-image">
 	    <img src="${card.icon}" class="img-responsive">
 	  </div>
