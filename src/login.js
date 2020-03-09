27606d1cadd28389ff06097290eaf21cd572fcc3
Enmanuel <54204155+emtes@users.noreply.github.com>
2020-03-09 14:43:29 -0400

    Update login.js

--- src/login.js
+++ src/login.js
@@ -2,7 +2,7 @@ function toSpotifyAccServices() {
   const creds = {
     client_id: "20072dfe69e4443ab2981969b901cbae",
     response_type: "token",
-    redirect_uri: "https%3A%2F%2Femtes.github.io%2Ftest%2Fmain.html",
+    redirect_uri: "https%3A%2F%2Femtes.github.io%2Freddify%2Fmain.html",
     scope: "user-top-read%20user-library-read"
   };
 
